9aee289d1cf79c1d7cc6dee8870ac718e469b9d8
KUBIAK <00michael.kubiak@students.kresa.org>
2026-04-08 13:35:03 -0400

    Isolated class into dialogue.js
    The showcase of its functionality or whatever is in main.js now

--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -75,13 +75,3 @@ export default class Dialogue {
         })
     }
 }
-
-const yahu_dialogue = new Dialogue("Benjamin Netanyahu", "../resources/images/netanyahu.png");
-await yahu_dialogue.show();
-await yahu_dialogue.reveal_multiple_texts(30, 1000, [
-    "Hi! Im your boss, Bejamin Netanyahu.",
-    "I have a very important task for you.",
-    "I need you to sell potions and make money for me.",
-    "Thanks brojob"
-]);
-yahu_dialogue.hide();
