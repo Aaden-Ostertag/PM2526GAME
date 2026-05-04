ffe1a840d90a57ea122caeb5266aa68fec60db70
m-kubes <michaelkubiak5@gmail.com>
2026-05-04 12:41:46 -0400

    Added yahu intro scene

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -7,3 +7,11 @@ SceneManager.add_scene(new Scene("Main Menu", () => {
 () => {
     console.log("Exiting Main Menu...");
 }));
+
+
+SceneManager.add_scene(new Scene("Yahu Intro", async () => {
+    const template_yahu_dialogue = await fetch("../resources/jsons/dialogue/template_yahu_dialogue.json"); 
+
+    const yahu_dialogue = document.querySelector("#yahu_dialogue");
+    await yahu_dialogue.conversation(template_yahu_dialogue);
+}));
